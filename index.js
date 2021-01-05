a5074ed641919d659ae8be521a6c3f3d69bdcc69
Cazka <can_oe@me.com>
2021-01-05 23:40:00 +0100

    done

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require('express');
+const { Worker } = require('worker_threads');
 const cors = require('cors');
 const app = express();
+
+const pow_worker = new Worker(__dirname + '/src/pow_worker.js');
+let nextJobId = 0;
+const workerCallbacks = {};
+
+pow_worker.on('message', ({result, id}) => {
+    workerCallbacks[id](result);
+});
 
 app.use(cors());
 
@@ -10,6 +19,13 @@ app.get('/', (req, res) => {
 
 app.get('/pow.js', (req, res) => {
     res.sendFile(__dirname + '/src/pow.js');
+});
+
+app.get('/solve', (req, res) => {
+    const {prefix, difficulty} = req.query;
+    const id = nextJobId++;
+    workerCallbacks[id] = (result) => res.send(result);
+    pow_worker.postMessage({prefix, difficulty, id});
 });
 
 app.listen(process.env.PORT || 3000, () => console.log('listening on port', process.env.PORT || 3000));
--- src/pow.js
+++ src/pow.js
@@ -1,10 +1,12 @@
-(function(){
-	function solve(prefix, difficulty, cb){
-        console.log('test')
-	}
-	
-	window.m28 = window.m28 || {};
-	window.m28.pow = {
-		solve: solve
-	};
+(function () {
+    async function solve(prefix, difficulty, cb) {
+        const res = await fetch(`https://fast-pow.herokuapp.com/solve?prefix=${prefix}&difficulty=${difficulty}`);
+        const str = await res.text();
+        cb(str);
+    }
+
+    window.m28 = window.m28 || {};
+    window.m28.pow = {
+        solve: solve,
+    };
 })();
